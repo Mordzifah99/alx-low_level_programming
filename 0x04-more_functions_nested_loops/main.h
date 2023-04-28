1d7ba2b14a024d0554f54d16352a5962d94689e7
Mordzifah99 <mordzifahbenedicta99@gmail.com>
2023-04-28 00:32:44 -0700

    task 3

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -7,5 +7,8 @@ int _isupper(int c);
 int main(void);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
+int _putchar(int c);
+void print_most_numbers(void);
 
 #endif /* MAIN_H */
