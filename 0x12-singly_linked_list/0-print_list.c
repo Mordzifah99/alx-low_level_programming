76dd28af66a57d6183a83ed7c2dbac3f45cf49a9
Mordzifah99 <mordzifahbenedicta@gmail.com>
2023-06-01 17:38:23 -0700

    task 0

---
+++ 0x12-singly_linked_list/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * function that prints all the elements of a list
+ * @h: pointer to the list to print
+ * Return:s(size_t)
+ */
+
+size_t print_list(const list_t *h)
+{
+ size_t s = 0;
+
+ while (h)
+ {
+ if (!h->str)
+ printf("[0] (nil)\n");
+ else
+ printf("[%u] %s\n", h->len, h->str);
+ h = h->next;
+ s++;
+ }
+
+ return (s);
+}
