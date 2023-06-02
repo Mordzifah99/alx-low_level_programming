ea7064a4b805e09cfc14c1fda53bf6f82ee642c8
Mordzifah99 <mordzifahbenedicta@gmail.com>
2023-06-01 17:54:44 -0700

    task 1

--- 0x12-singly_linked_list/lists.h
+++ 0x12-singly_linked_list/lists.h
@@ -2,10 +2,12 @@
 #define LISTS_H
 
 /**
- * function that prints all the element of a list
- * @next: leads to the next node
- * @str: malloc string
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
  * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 
 typedef struct list_s
