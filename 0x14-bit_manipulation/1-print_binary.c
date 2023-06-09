ad79908d470a87c35cc128b2be554c0f4af46568
Mordzifah99 <mordzifahbenedicta@gmail.com>
2023-06-09 05:29:50 -0700

    a function that prints the binary representation of a number

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,30 @@
 #include "main.h"
 
 /**
- *
+ * print_binary:function that prints the binary representation of a number
+ * @n: numbers to print in binary
+ * Return: the converted number
+ */
+void print_binary(unsigned long int n)
+{
+	int j, count = 0;
+	unsigned long int fresh;
+
+	for (j = sizeof(unsigned long int) * 8 - 1; j >= 0; j--)
+	{
+		fresh = n >> j;
+		if (fresh & 1)
+		{
+			_putchar('1');
+			count++;
+		} else if (count)
+		{
+			_putchar('0');
+		}
+	}
+
+		if (!count)
+		{
+			_putchar('0');
+		}
+}
